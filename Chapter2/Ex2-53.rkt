3183d0cfd1edcf3eae68f71ccb4a946f4a6dc4b1
Paszyński,Mateusz M.I <m.paszynski@student.fontys.nl>
2022-04-19 17:54:09 +0200

    Exercise 2.53 done

---
+++ Chapter2/Ex2-53.rkt
@@ -0,0 +1,18 @@
+;;; Exercise 2.53
+;;; What would interpreter print in response to evaluating each of these expressions?
+
+#lang sicp
+
+(list 'a 'b 'c) (a b c)
+
+(list (list 'george)) ((george))
+
+(cdr '((x1 x2)(y1 y2))) ((y1 y2))
+
+(cadr '((x1 x2) (y1 y2))) (y1 y2)
+
+(pair? (car '(a short list))) ;False
+
+(memq 'red '((red shoes) (blue socks))) ; False
+
+(memq 'red '(red shoes blue socks)) ;(red shoes blue socks)+
